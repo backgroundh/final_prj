15afaf5fb8bc5bfd8eb27d144f86aefc71f0353d
backgroundh <bgh4350@gmail.com>
2022-10-30 05:12:30 +0900

    index

---
+++ js/fixed.js
@@ -0,0 +1,18 @@
+//alert("테스트");
+$(function(){
+    $('#logo').click(
+        function(){
+            $('#logo').hide();
+        }
+    );
+    // $("#pop_bnt > a").click(
+    //     function(){
+    //         $('.on').hide()
+    //     }
+    // );
+    // $('nav>ul>li').hover(
+    // function(){
+    //     $('#subNav').stop().slideDown();
+    // }
+    // );
+});
